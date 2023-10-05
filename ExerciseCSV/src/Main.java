73e7f6201fd173f750f1a24092e689c86ccdc8fb
Relampag0o <josemariar.l17@gmail.com>
2023-10-05 19:34:36 +0200

    .

--- ExerciseCSV/src/Main.java
+++ ExerciseCSV/src/Main.java
@@ -77,8 +77,12 @@ public class Main {
             bfw.write((students.get(students.size() / 2).getCalification()));
             bfw.newLine();
             System.out.println("Students that passed: " + calculatePassedAndFailledStudents(students)[0] + "Students who failled: " + calculatePassedAndFailledStudents(students)[1]);
+<<<<<<< HEAD
             bfw.write("Students that passed: " + calculatePassedAndFailledStudents(students)[0] + "%" + " Students who failled: " + calculatePassedAndFailledStudents(students)[1] + "%");
             
+=======
+bfw.write("Students that passed: " + calculatePassedAndFailledStudents(students)[0] + "%" + " Students who failled: " + calculatePassedAndFailledStudents(students)[1] + "%");
+>>>>>>> parent of 4cce797 (.)
         } catch (Exception e) {
             System.out.println("Error..");
         }
