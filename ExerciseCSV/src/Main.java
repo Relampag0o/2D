4a696cf2b5f2a329a9fef1533aafea7209bbe5e2
Relampag0o <josemariar.l17@gmail.com>
2023-10-06 18:24:42 +0200

    almost finished

--- ExerciseCSV/src/Main.java
+++ ExerciseCSV/src/Main.java
@@ -46,12 +46,33 @@ public class Main {
 
     }
 
+    public static String listSpecialties(LinkedList<Student> students) {
+        LinkedList<String> specialties = new LinkedList<String>();
+        String s = "";
+        for (Student st : students) {
+            if (!specialties.contains(st.getSpeciality()))
+                specialties.add(st.getSpeciality());
+
+
+        }
+        for (int i = 0; i < specialties.size(); i++) {
+            if (i == specialties.size() - 1)
+                s += specialties.get(i) + "";
+            else
+                s += specialties.get(i) + ", ";
+
+        }
+
+        return s;
+
+    }
+
     public static String calitifactionsBySpecialty(LinkedList<Student> students, String speciality) {
-        int[] califications = new int[3];
+        double[] califications = new double[3];
         int amountOfStudentsBySpeciality = 0;
         for (Student s : students) {
             if (s.getSpeciality().equalsIgnoreCase(speciality)) {
-                califications[0] += s.getCalification();
+                califications[0] += (double) s.getCalification();
                 amountOfStudentsBySpeciality++;
                 if (s.getCalification() >= 5)
                     califications[1] = califications[1] + 1;
@@ -60,17 +81,52 @@ public class Main {
             }
 
         }
-        return "Califications by speciality " + speciality + " : Average:  " + califications[0] / amountOfStudentsBySpeciality +
-                " Students that passed: " + califications[1] * 100 / amountOfStudentsBySpeciality + " Students that failed: " +
-                califications[2] * 100 / amountOfStudentsBySpeciality;
+        return speciality + ": " + '\n' + " Average: " + califications[0] / amountOfStudentsBySpeciality +
+                '\n' + " Students that passed: " + Math.round(califications[1] * 100 / amountOfStudentsBySpeciality) + "% " + '\n' + " Students that failed: " +
+                Math.round(califications[2] * 100 / amountOfStudentsBySpeciality) + "%";
+    }
+
+    public static int countStudentsByOccurrence(LinkedList<Student> students, String occurrence) {
+        int counter = 0;
+        for (Student s : students) {
+            if (s.getName().contains(occurrence)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public static String searchNamesWithTilde(LinkedList<Student> students,File f,BufferedWriter bfw ) {
+        Set<String> namesWithTilde = new TreeSet<>();
+        int[] asciiCodes = {225, 233, 237, 243, 250};
+        // im using the TreeSet because it automatically sorts the list.
+        // i couldve used the literally characters to compare them with the name but using ascii codes looks better! (char[] tildes = {á,é,í..};)
+        for (Student s : students) {
+            String name = s.getName();
+            for (int i = 0; i < name.length(); i++) {
+                Character c = name.charAt(i);
+                for (int asciiCode : asciiCodes) {
+                    if ((int) c == asciiCode)
+                        namesWithTilde.add(s.getName());
+                }
+
+            }
+
+
+        }
+        for (String w : namesWithTilde) {
+            System.out.println("Name: " + w);
+        }
+
+        return "";
     }
 
     public static void main(String[] args) {
-        String pathsource = "C:\\Users\\Jose\\Desktop\\alumnos.csv";
-        String pathdest = "C:\\Users\\Jose\\Desktop\\output.txt";
+        String pathsource = "C:\\Users\\Josem\\Desktop\\alumnos.csv";
+        String pathdest = "C:\\Users\\Josem\\Desktop\\output.txt";
         File f1 = new File(pathsource);
         File f2 = new File(pathdest);
-        String s = "";
+
 
         try (BufferedReader bfr = new BufferedReader(new FileReader(f1, StandardCharsets.UTF_8));
              BufferedWriter bfw = new BufferedWriter(new FileWriter(f2, StandardCharsets.UTF_8))) {
@@ -98,10 +154,18 @@ public class Main {
             bfw.newLine();
             System.out.println("Students that passed: " + Math.round(calculatePassedAndFailedStudents(students)[0]) + "%" + '\n' + "Students who failled: " + Math.round(calculatePassedAndFailedStudents(students)[1]) + "%");
             bfw.write("Students that passed: " + Math.round(calculatePassedAndFailedStudents(students)[0]) + "%" + '\n' + "Students who failled: " + Math.round(calculatePassedAndFailedStudents(students)[1]) + "%");
+            bfw.newLine();
+            System.out.println(listSpecialties(students));
+            bfw.write("List of specialites: " + listSpecialties(students));
+            bfw.newLine();
             System.out.println(calitifactionsBySpecialty(students, "ciencias"));
             System.out.println(calitifactionsBySpecialty(students, "letras"));
             System.out.println(calitifactionsBySpecialty(students, "artes"));
-
+            bfw.write(calitifactionsBySpecialty(students, "ciencias") + '\n');
+            bfw.write(calitifactionsBySpecialty(students, "letras") + '\n');
+            bfw.write(calitifactionsBySpecialty(students, "artes") + '\n');
+            System.out.println(countStudentsByOccurrence(students, "j"));
+            searchNamesWithTilde(students,);
 
         } catch (Exception e) {
             System.out.println("Error..");
