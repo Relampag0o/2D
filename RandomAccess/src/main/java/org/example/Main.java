a673b4ae4c41fe99b5ff2205a934c0ef07e732e6
Relampag0o <josemariar.l17@gmail.com>
2023-10-10 11:29:09 +0200

    ...

--- RandomAccess/src/main/java/org/example/Main.java
+++ RandomAccess/src/main/java/org/example/Main.java
@@ -6,60 +6,89 @@ import java.io.*;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
-        public static void createIndex (String src, String dst){
-            File f = new File(src);
-            File f2 = new File(dst);
-            try(BufferedReader bfr = new BufferedReader(new FileReader(f));
-                BufferedWriter bfw = new BufferedWriter(new FileWriter(dst))){
-            String line =bfr.readLine();
+    public static void createIndex(String src, String dst) {
+        File f = new File(src);
+        File f2 = new File(dst);
+        try (BufferedReader bfr = new BufferedReader(new FileReader(f));
+             BufferedWriter bfw = new BufferedWriter(new FileWriter(dst))) {
+            String line = bfr.readLine();
             int index = line.getBytes().length;
-            bfw.write("DNI: " + index + '\n');
-                System.out.println("DNI: " + index + '\n');
 
-            while ((line=bfr.readLine())!=null){
+            while ((line = bfr.readLine()) != null) {
                 String[] words = line.split(";");
-                index+=line.getBytes().length;
-                bfw.write("DNI: " + words[0] + "" + index + '\n');
-                System.out.println("DNI: " + words[0] + "" + index + '\n');
 
+                bfw.write(words[0] + ";" + index + '\n');
+                System.out.println(words[0] + ";" + index + '\n');
+                index += line.getBytes().length;
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error...");
+        }
+    }
+
+    public static void searchInfo(String dni, String indexFile, String dbSrc) {
+
+        File f = new File(indexFile);
+        try {
+            BufferedReader bfr = new BufferedReader(new FileReader(f));
+            String line = "";
+            int index = 0;
+            boolean found = false;
+            while ((line = bfr.readLine()) != null && !found) {
+                String[] words = line.split(";");
+                if (words[0].equalsIgnoreCase(dni)) {
+                    index = Integer.parseInt(words[1]);
+                    found = true;
+                }
+            }
+
+            if (found) {
+                try {
+                    RandomAccessFile raf = new RandomAccessFile(dbSrc, "r");
+                    raf.seek(index);
+                    FileReader fr = new FileReader(raf.getFD());
+                    BufferedReader bfr2 = new BufferedReader(fr);
+                    System.out.println(bfr2.readLine());
+                } catch (Exception e) {
+                    System.out.println("Error..");
+                }
 
 
             }
 
 
-
-            }catch(Exception e){
-                System.out.println("Error...");
-            }
-
-
+        } catch (Exception e) {
+            System.out.println("error.");
 
         }
+    }
 
-        public void searchInfo (String dni, String indexFile){
-
-        }
 
     public static void main(String[] args) {
 
-
-            //App app = new App();
-            if (args.length == 3) {
-                switch (args[0]) {
-                    case "-g":
-                        System.out.println(args[1]+args[2]);
-                        createIndex(args[1],args[2]);
-                        break;
-                    case "-f":
-                        //app.buscarInfo(args[1], args[2]);
-                        break;
-                    default:
-                        System.err.println("Not supported opperattion");
-                        break;
-                }
-            } else {
-                System.err.println("The amount of args is not valid. Try again.");
+        if (args.length == 3) {
+            switch (args[0]) {
+                case "-g":
+                    createIndex(args[1], args[2]);
+                    break;
+                default:
+                    System.err.println("Not supported opperattion");
+                    break;
             }
-        }
+        } else if (args.length == 4) {
+            switch (args[0]) {
+                case "-f":
+
+                    // -f 84939447V C:\Users\Jose\Desktop\output2.txt C:\Users\Jose\Downloads\db.csv
+                    searchInfo(args[1], args[2], args[3]);
+
+                    break;
+
+            }
+        } else
+            System.out.println("Invalid amount of arguments..");
     }
+
+}
 
