28f3c9f667cbd149db4d1fa8188ffc8eca64ff3e
Relampag0o <josemariar.l17@gmail.com>
2023-10-18 14:13:52 +0200

    .

--- ProcessSimulator/src/Simulator.java
+++ ProcessSimulator/src/Simulator.java
@@ -2,35 +2,29 @@ import java.lang.reflect.Array;
 import java.sql.SQLOutput;
 import java.util.*;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Simulator {
     public ArrayList<MyProcess> processes;
 
-
     public Simulator() {
         this.processes = new ArrayList<MyProcess>();
+        generateProcesses();
 
     }
 
     public void generateProcesses() {
-/*
+
         Random r = new Random();
         for (int i = 0; i < 5; i++) {
             processes.add(new MyProcess("pid_" + i, r.nextInt(7), r.nextInt(5)));
         }
-*/
 
-        processes.add(new MyProcess("p1", 0, 3));
-        processes.add(new MyProcess("p2", 2, 4));
-        processes.add(new MyProcess("p3", 3, 2));
-        processes.add(new MyProcess("p4", 3, 1));
 
     }
 
     public void playFCFS() {
         Collections.sort(processes);
         simulation();
+        showTableProcesses();
     }
 
     public void playSJF() {
@@ -75,6 +69,7 @@ public class Simulator {
                 e.printStackTrace();
             }
         }
+        showTableProcesses();
     }
 
     public void showTableProcesses() {
@@ -90,13 +85,9 @@ public class Simulator {
     public static void main(String[] args) {
 
         Simulator sim = new Simulator();
-        sim.generateProcesses();
-
         System.out.println("PLAYING FCFS:");
         sim.playFCFS();
-
         System.out.println("-----");
-
         System.out.println("PLAYING SJF: ");
         sim.playSJF();
 
